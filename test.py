e6affd7cf5e1cfe52d20114ab125ed917d813a98
landersantdas <39044659+landersantdas@users.noreply.github.com>
2018-05-16 21:01:27 +0800

    Update test.py
    Added vote-count, asked-by, edit-by on question and
    vote-count, answered-by, edit-by on answers

--- test.py
+++ test.py
@@ -4,27 +4,116 @@ import re
 
 site = req.Request('https://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array')
 page = req.urlopen(site)
-soup = BeautifulSoup(page, 'html.parser') # from url
+soup = BeautifulSoup(page, 'html.parser') 
 
 
 final = {}
 answers = []
-question = {}
+finalQuestion = {}
+voteCount = []
+
+aName = []
+aDate = []
+editDate = []
+editName = []
+
+
+askedObj = {}
+askedEditObj = {}
+answeredObj = {}
+answeredEditObj = {}
 
 #get all text inside post-text div
 info = soup.findAll('div',attrs={"class":"post-text"})
 for ans in info:
     answers.append(ans.text)
 
+#get vote count
+info2 = soup.findAll('span',attrs={"class":"vote-count-post"})
+for ans2 in info2:
+    voteCount.append(ans2.text)
+
+
+dumaan = False
+ctr = 0
+noName = False
+#get date&name of edited and not of asked and answered 
+info3 = soup.findAll('div',attrs={"class":"post-signature grid--cell fl0"})
+for ans3 in info3:
+	if (ans3.select('.user-action-time')[0].a is None):		
+		if (ans3.select('.user-details')[0].a is not None):
+			aName.append(ans3.select('.user-details')[0].a.string)
+			aDate.append(ans3.select('.user-action-time')[0].span.string)
+
+			if (dumaan):
+				dumaan = False
+			else:
+				editName.append("none")
+				editDate.append("none")
+
+			if (noName):
+				editName.append(aName[-1])
+				noName = False
+
+	else:
+		dumaan = True
+		if (ans3.select('.user-details')[0].a is not None):
+			editName.append(ans3.select('.user-details')[0].a.string)
+			editDate.append(ans3.select('.user-action-time')[0].span.string)
+		else: 
+			noName = True
+			editDate.append(ans3.select('.user-action-time')[0].span.string)
+	ctr = ctr + 1
+	#print(ctr)
+
+editName[0] = "none"
+editDate[0] = "none"
+
+#print(str(editName) + "\n" +str(editDate))
+
+# get the username and dateasked of the person who asked
+info4 = soup.findAll('div',attrs={"class":"post-signature owner grid--cell fl0"})
+for ans4 in info4:
+	if (ans4.select('.user-action-time')[0].a is None):		
+		if (ans4.select('.user-details')[0].a is not None):
+			askedObj['name'] = ans4.select('.user-details')[0].a.string
+			askedObj['date'] = ans4.select('.user-action-time')[0].span.string
+
+
+askedEditObj['name'] = editName[0]
+askedEditObj['date'] = editDate[0]
+
+
 #question
-question['title'] = soup.title.text
-question['body'] = answers[0]
+finalQuestion['title'] = soup.title.text
+finalQuestion['body'] = answers[0]
+finalQuestion['vote-count'] = voteCount[0]
+finalQuestion['asked-by'] = askedObj
+finalQuestion['edited-by'] = askedEditObj
 
-final['question'] = question 
+final['question'] = finalQuestion 
 
-#delete the question and add answers to final
+
+#delete the questions data and add answers to final
 del answers[0]
-final['answers'] = answers
+del voteCount[0]
+#del editName[0]
+#del editDate[0]
+
+
+semi = []
+count = 0
+
+while (count < len(answers)):
+	finalAnswer = {}
+	finalAnswer['answer'] = answers[count]
+	finalAnswer['vote-count'] = voteCount[count]
+	finalAnswer['answered-by'] = {'name' : aName[count], 'date' : aDate[count]}
+	finalAnswer['edited-by'] = {'name' : editName[count], 'date' : editDate[count]}
+	semi.append(finalAnswer)
+	count = count + 1
+
+final['answers'] = semi
 
 
 print(final)
