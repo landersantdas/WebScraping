f9d701464918441916282a1cf9d8dad7fea66abb
Lander Santdas <39044659+landersantdas@users.noreply.github.com>
2018-05-21 02:01:20 +0800

    Update jsonsaver.py
    added 2 more functions

--- Scraping-Module/jsonsaver.py
+++ Scraping-Module/jsonsaver.py
@@ -6,10 +6,17 @@ def save(parseResult):
 	savefile = filedialog.asksaveasfile(defaultextension=".json", mode = 'w', title = 'Select where to save the result')
 	if (savefile is not None):
 		json.dump(parseResult, savefile, indent = 4)
-		print("\nParsing complete. ")
 		print("\nResult saved successfully!")
 	else:
 		with open('result.json', 'w') as fp:
 			json.dump(parseResult, fp, indent = 4)
-			print("\nParsing complete.")
 			print("\nThe result is saved on your default folder as 'result.json'.")
+
+def saveDefault(parseResult):
+	with open('result.json', 'w') as fp:
+		json.dump(parseResult, fp, indent = 4)
+		print("\nResult saved successfully as 'result.json'.")	
+
+def printResult(parseResult):
+	print ("Parsing result:\n")
+	print (parseResult)
