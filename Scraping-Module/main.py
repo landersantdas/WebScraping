f50875e09b7b10355e4fbbcfcf9e0bfb93a1b787
Lander Santdas <39044659+landersantdas@users.noreply.github.com>
2018-05-21 02:03:25 +0800

    Update main.py
    Added more options etc.

--- Scraping-Module/main.py
+++ Scraping-Module/main.py
@@ -6,7 +6,7 @@ import jsonsaver
 
 
 cont = 'y'
-
+ 
 while (cont == 'y'):
 	url = input("\nEnter URL: ")
 	if ("https://stackoverflow.com/questions" in url):
@@ -14,9 +14,24 @@ while (cont == 'y'):
 		site = req.Request(url)
 		page = req.urlopen(site)
 		soup = BeautifulSoup(page, 'html.parser') 
-
 		result = stackoverflowparser.parse(soup)
-		jsonsaver.save(result)
+		if (result is not None):
+			print ("\nParsing complete!")
+			print("\nWhat do you want to do with the result?\n\n[1] - Select where to save the it\n[2] - Save to default folder\n[3] - Print in console") 
+			valid = False
+			while (not valid):
+				where = input("= ")
+				if (where == '1'):
+					jsonsaver.save(result)
+					valid = True
+				elif (where == '2'):
+					jsonsaver.saveDefault(result)
+					valid = True
+				elif (where == '3'):
+					jsonsaver.printResult(result)
+					valid = True
+				else:
+					print('Invalid Input')
 		exit()
 	else:
 		cont = input("\nInvalid URL\n\nDo you want to continue? [y/n]: ")
